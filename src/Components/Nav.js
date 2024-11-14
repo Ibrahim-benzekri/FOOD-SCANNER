0ee211f72f58c14574540cd03a75688d0b486b2e
Ibrahim BENZEKRI ARBAI <123778755+Ibrahim-benzekri@users.noreply.github.com>
2024-11-14 21:11:56 +0100

    nav bar adjust

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -11,7 +11,7 @@ const Nav = ({nav}) => {
   return (
     <header className={`w-full ${nav ? "fixed bg-black duration-300 top-0 left-0 z-30" : "absolute"}`}>
       <nav className='max-w-[1540px] m-auto py-6 px-6 max-lg:px-12 flex justify-between items-center gap-14 max-xl:gap-5 max-sm:py-4 max-sm:px-6'>
-        <a href="#home" className=' font-semibold text-5xl flex justify-center items-center text-white gap-1'>
+        <a href="#home" className=' font-semibold text-3xl flex justify-center items-center text-white gap-1'>
           <img src={logo} alt="logo" width={60} height={60}/>
           FITNESS SCANNER
         </a>
